8902e7533d2dab9e79627fb39d45e093d64730dc
Kennedy Waweru <Kennedywaweru17@gmail.com>
2021-10-08 14:04:34 +0300

    uploaded website files

---
+++ js/testimonials.js
@@ -0,0 +1,31 @@
+
+/*$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        pagination:true,
+        navigation:true,
+        navigationText:["",""],
+        slideSpeed:1000,
+        autoPlay:true
+    });
+});*/
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1.2
+        },
+        1000:{
+            items:1.5
+        }
+    }
+})
